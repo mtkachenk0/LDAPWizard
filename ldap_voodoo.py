85b23aa2b6f9c7faae33f909523b456e11360823
Maxim Tkachenko <tkacenko.maxim@gmail.com>
2017-02-23 19:34:14 +0300

    Update ldap_voodoo.py

--- ldap_voodoo.py
+++ ldap_voodoo.py
@@ -1,11 +1,14 @@
 import logging
-from .conn_manager import LDAPConnectionManager
+from .conn_manager import LDAPVoodoo
 from addendum.exceptions import LDAPInvalidFullDN
 
 log = logging.getLogger(__name__)
+
 AVAILABLE_LDAP_SCOPE = ('client', 'stuff', 'supervisor', 'sys')
 
+
 class LDAPWizard(LDAPVoodoo):
+
     @staticmethod
     def prettify_full_dn(full_dn):
         """
@@ -22,7 +25,7 @@ class LDAPWizard(LDAPVoodoo):
             }
         except Exception as ex:
             raise LDAPInvalidFullDN(ex)
-            
+
     def get_full_dn(self, uid, scope):
         """
         This method will return a list of client dn entries, or None in case that there is no such an entry.
@@ -31,6 +34,7 @@ class LDAPWizard(LDAPVoodoo):
         :return: normalised result or None
         """
         assert scope in AVAILABLE_LDAP_SCOPE
+
         log.debug('Looking for customer: %s', uid)
         search_result = self.search(base_dn="ou=%s," % scope + self.base_dn,
                                     search_filter="(cn=%s)" % uid,
@@ -44,9 +48,10 @@ class LDAPWizard(LDAPVoodoo):
                 'pretty': self.prettify_full_dn(full_dn),
                 'normal': full_dn
                 }
-                
+
     def get_profile_data(self, full_dn, attributes=None):
         """
+
         :param full_dn: Distinguished Name (address) of the entry
         :param attributes: List of attribute names
         :return:
@@ -56,6 +61,7 @@ class LDAPWizard(LDAPVoodoo):
                 full_dn = full_dn['normal']
             except KeyError as ex:
                 return LDAPInvalidFullDN(ex)
+
         if isinstance(attributes, (list, tuple)):
             attributes = [attribute.encode("utf-8") for attribute in attributes]
         elif isinstance(attributes, basestring):
@@ -63,14 +69,17 @@ class LDAPWizard(LDAPVoodoo):
         else:
             # to retrieve all filled attributes ;)
             attributes = []
+
         return self.search(base_dn=full_dn, search_filter=None, retrieve_attributes=attributes)
-        
+
     def set_profile_data(self, full_dn, attributes):
+
         if not isinstance(full_dn, basestring):
             try:
                 full_dn = full_dn['normal']
             except KeyError as ex:
                 return LDAPInvalidFullDN(ex)
+
         stringify_attributes = {}
         for key, value in attributes.iteritems():
             # for unicode keys, make them utf8 strings
@@ -82,9 +91,11 @@ class LDAPWizard(LDAPVoodoo):
             # if it is a list, go through its contents
             elif isinstance(value, list):
                 value = [item.encode('utf-8') if isinstance(item, unicode) else item for item in value]
+
             stringify_attributes[key] = value
+
         self.modify_attributes(full_dn, attributes)
-        
+
 w = LDAPWizard({
                 'basedn': 'dc=co,dc=com',
                 'user': "cn=admin,dc=co,dc=com",
@@ -93,4 +104,5 @@ w = LDAPWizard({
                 'host': '127.0.0.1',
                 'port': 389
             })
+
 print w.get_full_dn('ldap_wizard', 'sys')
